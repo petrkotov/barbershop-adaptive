d83bb0c2d4d944a677bffb1ac262c39283832f33
petrkotov <30962854+petrkotov@users.noreply.github.com>
2017-12-13 22:21:45 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,8 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
+var navLogin = document.querySelector(".main-nav__user-login");
+var navShow = document.querySelector(".modal-login");
+var navClose = document.querySelector(".modal-login__close");
 navMain.classList.remove("main-nav--nojs");
 navToggle.addEventListener("click", function(){
   if (navMain.classList.contains("main-nav--closed")) {
@@ -10,4 +13,12 @@ navToggle.addEventListener("click", function(){
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
   }
+})
+
+navLogin.addEventListener("click", function(){
+    navShow.classList.add("modal-login--show");
+})
+
+navClose.addEventListener("click", function(){
+    navShow.classList.remove("modal-login--show");
 })
